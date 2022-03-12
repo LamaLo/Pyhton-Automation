1b6ca1b56afe6b372b6f4ae2523ad95455cd84e9
user1 <user1@rhost1.localnet.com>
2022-03-12 11:36:05 +0200

    Simple read/write file

---
+++ fileIO.py
@@ -0,0 +1,13 @@
+f = open('inputFile.txt', 'r')
+failFile = open('FailFile.txt', 'w')
+passFile = open('PassFile.txt', 'w')
+for line in f:
+	line_split = line.split()
+	if line_split[2] == 'P':
+		passFile.write(line)
+	else:
+		failFile.write(line)	
+f.close()
+passFile.close()
+failFile.close()
+
